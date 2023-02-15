760bfbc71b96ad85a8e00ca50d8e6c146ba33c48
tejks <sarmatys.programmer@gmail.com>
2023-02-15 22:49:31 +0100

    feat: trade page grid

--- src/routes/trade/+page.svelte
+++ src/routes/trade/+page.svelte
@@ -3,7 +3,28 @@
 </script>
 
 <div class="trade-page">
+    <!-- STATISTICS -->
+    <div class="statistics-section">
+
+    </div>
+
+    <!-- TOKEN LIST -->
+    <div class="token-list-section">
+
+    </div>
+
+    <!-- CHART -->
     <div class="lightweight-section">
         <Lightweight/>
+    </div>
+
+    <!-- BUY / SELL -->
+    <div class="buy-sell-section">
+
+    </div>
+
+    <!-- POSITIONS LIST -->
+    <div class="positions-section">
+
     </div>
 </div>

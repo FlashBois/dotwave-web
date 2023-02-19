eaaa45ec061f2ac942e6f5082ed4b19a198a2ef0
tejks <sarmatys.programmer@gmail.com>
2023-02-19 23:58:28 +0100

    feat: add image lazy loader

---
+++ src/components/Image/ImageLoader.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let src: string;
+	export let alt: string;
+
+	import IntersectionObserver from './IntersectionObserver.svelte';
+	import Image from './Image.svelte';
+</script>
+
+<IntersectionObserver once={true} let:intersecting>
+	{#if intersecting}
+		<Image {alt} {src} />
+	{/if}
+</IntersectionObserver>

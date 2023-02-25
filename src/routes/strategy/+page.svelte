6514b7c77312a08707e219a194b63fbd681694bb
tejks <sarmatys.programmer@gmail.com>
2023-02-25 22:55:17 +0100

    feat: create strategy layout

---
+++ src/routes/strategy/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import StrategyTable from "$components/Strategy/Table/Table.svelte";
+</script>
+
+<div class="strategy-page">
+    <div class="filters-section">
+
+    </div>
+    <div class="strategy-table-section">
+        <StrategyTable />
+    </div>
+</div>

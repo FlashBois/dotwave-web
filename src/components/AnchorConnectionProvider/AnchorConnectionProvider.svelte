72d770ec469bc9b3bc33241a7d6713fd7dc7f50c
tejks <sarmatys.programmer@gmail.com>
2023-02-28 23:34:34 +0100

    feat: create anchor provider

---
+++ src/components/AnchorConnectionProvider/AnchorConnectionProvider.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { Connection, PublicKey } from '@solana/web3.js';
+	import type { Commitment, ConnectionConfig } from '@solana/web3.js';
+	import { AnchorProvider, Program } from '@project-serum/anchor';
+	import { walletStore, type WalletStore } from '$src/stores/walletStore';
+	import { anchorStore } from '$src/stores/anchorStore';
+	import { IDL } from '$src/utils/Idl/protocol';
+
+	export let
+		network: string,
+		config: Commitment | ConnectionConfig | undefined = 'processed';
+
+	export const PROGRAM_ID = new PublicKey("3wnPHyMvFaAMQoHYmkQ52erfYocW5f4GmkmdNzu3Couv");
+	const connection = new Connection(network, config);
+
+	function defineProgramAndProvider(walletStore: WalletStore) {
+		let { signTransaction, signAllTransactions, publicKey } =
+			walletStore;
+
+		const provider = new AnchorProvider(
+			connection,
+			{
+				publicKey: publicKey!,
+				signAllTransactions: signAllTransactions!,
+				signTransaction: signTransaction!
+			},
+			{
+				preflightCommitment: 'processed'
+			}
+		);
+
+		const program = new Program(IDL, PROGRAM_ID , provider);
+
+		anchorStore.set({
+			connection,
+			program,
+			network
+		})
+	}
+
+	$: $walletStore && $walletStore.publicKey && defineProgramAndProvider($walletStore);
+</script>
+
+<slot />

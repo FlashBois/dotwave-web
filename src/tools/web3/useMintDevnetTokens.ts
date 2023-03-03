c9a527cb1e630467a93694620740130e80125505
tejks <sarmatys.programmer@gmail.com>
2023-03-03 21:19:49 +0100

    feat: update tokens mint and devnet list

--- src/tools/web3/useMintDevnetTokens.ts
+++ src/tools/web3/useMintDevnetTokens.ts
@@ -4,7 +4,6 @@ import {
 	createAssociatedTokenAccountInstruction,
 	createMintToInstruction,
 	getAssociatedTokenAddress,
-	mintTo
 } from '@solana/spl-token';
 import { Keypair, PublicKey, Transaction, type Connection } from '@solana/web3.js';
 import { get } from 'svelte/store';
@@ -36,8 +35,10 @@ export async function useMintDevnetTokens(connection: Connection, wallet: Wallet
 	const tokens = [new PublicKey(vaults.quote_token(0))];
 
 	for (let i = 0; i < vaults.vaults_len() && i < MAX_TOKENS_TO_MINT; i++) {
-		tokens.push(new PublicKey(vaults.base_token(0)));
+		tokens.push(new PublicKey(vaults.base_token(i)));
 	}
+
+	console.log('Minting tokens', tokens);
 
 	const accountAddresses = await Promise.all(
 		tokens.map((token) => getAssociatedTokenAddress(token, walletAddress))
@@ -50,12 +51,11 @@ export async function useMintDevnetTokens(connection: Connection, wallet: Wallet
 	const tx = new Transaction();
 
 	accountAddresses
+		.map((address, i) =>
+			createAssociatedTokenAccountInstruction(walletAddress, address, walletAddress, tokens[i])
+		)
 		.filter((_, i) => !accounts[i])
-		.forEach((address, i) => {
-			tx.add(
-				createAssociatedTokenAccountInstruction(walletAddress, address, walletAddress, tokens[i])
-			);
-		});
+		.forEach((instruction) => tx.add(instruction));
 
 	accountAddresses.forEach((address, i) => {
 		tx.add(createMintToInstruction(tokens[i], address, minter.publicKey, 1e7));

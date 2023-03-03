d5645b1a9ae964b2ccd95f92fbb493ed99c2278d
tejks <sarmatys.programmer@gmail.com>
2023-03-03 20:25:45 +0100

    feat: create repay instruction

---
+++ src/tools/instructions/useRepay.ts
@@ -0,0 +1,28 @@
+import type { Protocol } from '$src/utils/Idl/protocol';
+import type { BN, Program } from '@project-serum/anchor';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import type { PublicKey, TransactionInstruction } from '@solana/web3.js';
+
+interface IRepayAccounts {
+	state: PublicKey;
+	vaults: PublicKey;
+	accountBase: PublicKey;
+	statement: PublicKey;
+	signer: PublicKey;
+	reserveBase: PublicKey;
+}
+
+export async function useBorrow(
+	program: Program<Protocol>,
+	vaultId: number,
+	accounts: IRepayAccounts,
+	amount: BN
+): Promise<TransactionInstruction> {
+	return await program.methods
+		.repay(vaultId, amount)
+		.accountsStrict({
+			...accounts,
+			tokenProgram: TOKEN_PROGRAM_ID
+		})
+		.instruction();
+}

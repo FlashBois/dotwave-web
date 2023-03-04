f552257ef35fc8ab64cc25d65f67495bd4a9a32d
tejks <sarmatys.programmer@gmail.com>
2023-03-04 01:02:34 +0100

    feat: create decimal input with validation

---
+++ src/components/Inputs/DecimalInput/DecimalInput.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let placeholder: string = '0.00';
+	export let value: number;
+
+	let className: string = '';
+	export { className as class };
+
+	function onInputChange(event: KeyboardEvent) {
+		const key = event.key;
+		const isPureDecimal = key.length > 1 || /^\d*\.?\d*$/.test(key);
+		const isControlKey = event.ctrlKey || event.altKey;
+		if (!isPureDecimal && !isControlKey) {
+			event.preventDefault();
+		}
+	}
+</script>
+
+<div class="input-box">
+	<input
+		class={className}
+		on:keydown={onInputChange}
+		on:change
+		type="text"
+		bind:value
+		{placeholder}
+		pattern="^[0-9-]*[.,]?[0-9]$"
+	/>
+</div>

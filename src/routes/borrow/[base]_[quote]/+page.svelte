0a3941237143286d13874b2e719aa2656db29df2
tejks <sarmatys.programmer@gmail.com>
2023-03-04 01:04:08 +0100

    feat: borrow component refactor

--- src/routes/borrow/[base]_[quote]/+page.svelte
+++ src/routes/borrow/[base]_[quote]/+page.svelte
@@ -1,7 +1,33 @@
-<script>
-	import Borrow from '$components/Borrow/Borrow.svelte';
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import Borrow from '$components/Borrow-Repay/Borrow/Borrow.svelte';
+	import Repay from '$components/Borrow-Repay/Repay/Repay.svelte';
+	import { protocolStateStore, type IVaultSupport } from '$src/stores/protocolStateStore';
+	import { onMount } from 'svelte';
+	import { get } from 'svelte/store';
+
+	$: ({ params } = $page);
+
+	let vaultSupport: IVaultSupport;
+
+	onMount(() => {
+		const protocolSateCopy = get(protocolStateStore);
+
+		const vault = protocolSateCopy.vaultsSupport.find(
+			(e) => e.baseTokenInfo.symbol == params.base && e.quoteTokenInfo.symbol == params.quote
+		);
+
+		if (vault) vaultSupport = vault;
+		else goto('RAY_USDC');
+	});
 </script>
 
 <div class="borrow-page">
-	<Borrow />
+	<div class="borrow-repay-section">
+		{#if vaultSupport}
+			<Borrow {vaultSupport} />
+			<Repay {vaultSupport} />
+		{/if}
+	</div>
 </div>

468d9146e9403420435dd987e6c2e0ccf69ff647
tejks <sarmatys.programmer@gmail.com>
2023-03-06 15:30:21 +0100

    feat: add borrow list

--- src/routes/borrow/[base]_[quote]/+page.svelte
+++ src/routes/borrow/[base]_[quote]/+page.svelte
@@ -2,32 +2,59 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Borrow from '$components/Borrow-Repay/Borrow/Borrow.svelte';
+	import BorrowList from '$components/Borrow-Repay/BorrowList/BorrowList.svelte';
 	import Repay from '$components/Borrow-Repay/Repay/Repay.svelte';
 	import { protocolStateStore, type IVaultSupport } from '$src/stores/protocolStateStore';
-	import { onMount } from 'svelte';
-	import { get } from 'svelte/store';
+	import { derived, get, writable } from 'svelte/store';
 
 	$: ({ params } = $page);
+	const borrowListVisible = writable(false);
 
-	let vaultSupport: IVaultSupport;
+	let backdrop: HTMLDivElement;
 
-	onMount(() => {
-		const protocolSateCopy = get(protocolStateStore);
+	$: vaultSupport = derived<[typeof page], IVaultSupport>([page], ([$page], set) => {
+		if ($page.params) {
+			const protocolSateCopy = get(protocolStateStore);
 
-		const vault = protocolSateCopy.vaultsSupport.find(
-			(e) => e.baseTokenInfo.symbol == params.base && e.quoteTokenInfo.symbol == params.quote
-		);
+			const vault = protocolSateCopy.vaultsSupport.find(
+				(e) => e.baseTokenInfo.symbol == params.base && e.quoteTokenInfo.symbol == params.quote
+			);
 
-		if (vault) vaultSupport = vault;
-		else goto('RAY_USDC');
+			if (vault) {
+				borrowListVisible.set(false);
+				set(vault);
+			}
+			else goto('RAY_USDC');
+		}
 	});
+
+	async function closeTokenList(e: MouseEvent) {
+		if (e.target === backdrop) {
+			borrowListVisible.set(false);
+		}
+	}
 </script>
 
 <div class="borrow-page">
 	<div class="borrow-repay-section">
-		{#if vaultSupport}
-			<Borrow {vaultSupport} />
-			<Repay {vaultSupport} />
+		<div
+			class="borrow-select-section"
+			on:click={() => {
+				borrowListVisible.set(true);
+			}}
+		>
+			{params.base}
+		</div>
+
+		{#if $vaultSupport}
+			<Borrow vaultSupport={$vaultSupport} />
+			<Repay vaultSupport={$vaultSupport} />
 		{/if}
 	</div>
+
+	{#if $borrowListVisible}
+		<div class="borrow-list-section" bind:this={backdrop} on:click={(e) => closeTokenList(e)}>
+			<BorrowList />
+		</div>
+	{/if}
 </div>

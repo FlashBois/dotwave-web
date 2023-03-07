13fd1032b42bf23e57a67cd4313f041b3515aeed
tejks <sarmatys.programmer@gmail.com>
2023-03-07 23:45:50 +0100

    faet: tools update

--- src/tools/web3/useCreateStatementProgramAddress.ts
+++ src/tools/web3/useCreateStatementProgramAddress.ts
@@ -2,13 +2,14 @@ import type { Program } from '@project-serum/anchor';
 import { PublicKey } from '@solana/web3.js';
 import * as anchor from '@project-serum/anchor';
 import type { Protocol } from '$src/utils/Idl/protocol';
+import { PROGRAM_ID } from '$src/stores/protocolStateStore';
 
 export const STATEMENT_SEED = 'statement';
 
 export function useCreateStatementProgramAddress(program: Program<Protocol>, user: PublicKey) {
 	const [statement] = PublicKey.findProgramAddressSync(
 		[Buffer.from(anchor.utils.bytes.utf8.encode(STATEMENT_SEED)), user.toBuffer()],
-		program.programId
+		PROGRAM_ID
 	);
 
 	return statement;

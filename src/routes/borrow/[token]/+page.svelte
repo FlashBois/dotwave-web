078967a4b03dfd94ca33606002b0952f3b983a96
tejks <sarmatys.programmer@gmail.com>
2023-03-09 13:21:54 +0100

    feat: borrow page refactor

--- src/routes/borrow/[token]/+page.svelte
+++ src/routes/borrow/[token]/+page.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import { protocolStateStore, type IVaultSupport } from '$src/stores/protocolStateStore';
+	import { userStore } from '$src/stores/userStore';
+	import { getDecimalFromBigint } from '$src/tools/decimal/getDecimalFromBigInt';
+	import { getCurrentUnixTime } from '$src/tools/getCurrentUnixTime';
+	import { derived, get } from 'svelte/store';
+
 	import Borrow from '$components/Borrow-Repay/Borrow/Borrow.svelte';
 	import BorrowRepayInfo from '$components/Borrow-Repay/BorrowRepayInfo/BorrowRepayInfo.svelte';
 	import Repay from '$components/Borrow-Repay/Repay/Repay.svelte';
 	import TokenList from '$components/TokenList/TokenList.svelte';
-	import { protocolStateStore, type IVaultSupport } from '$src/stores/protocolStateStore';
-	import { derived, get, writable } from 'svelte/store';
 
 	$: ({ params } = $page);
-	const borrowListVisible = writable(false);
+	$: ({ statement, statementBuffer } = $userStore);
+	$: ({ vaultsAccounts } = $protocolStateStore);
+
+	$: borrowListVisible = false;
 
 	$: vaultSupport = derived<[typeof page], IVaultSupport>([page], ([$page], set) => {
 		if ($page.params) {
@@ -20,19 +27,20 @@
 			);
 
 			if (vault) {
-				borrowListVisible.set(false);
+				borrowListVisible = false;
 				set(vault);
 			} else goto('RAY');
 		}
 	});
+	$: ({ id, baseTokenInfo, quoteTokenInfo } = $vaultSupport)
 
-	function onCloseTokenList() {
-		borrowListVisible.set(false);
-	}
+	$: maxBorrowAmount =
+		vaultsAccounts && statement
+			? getDecimalFromBigint(vaultsAccounts.max_borrow_for(id, statement.remaining_permitted_debt()), 6)
+			: undefined;
 
-	function onTokenListOpen() {
-		borrowListVisible.set(true);
-	}
+	$: userBorrowInfo = vaultsAccounts && statementBuffer ? vaultsAccounts.get_borrow_position_info(id, statementBuffer, getCurrentUnixTime()) : undefined
+	$: maxRepayAmount = userBorrowInfo ? getDecimalFromBigint(userBorrowInfo.borrowed_quantity, 6).add(getDecimalFromBigint(userBorrowInfo.owed_quantity, 6)) : undefined
 
 	function onTokenClick(token: string) {
 		goto(`${token}`);
@@ -42,16 +50,14 @@
 <div class="borrow-page">
 	<div class="borrow-repay-section">
 		{#if $vaultSupport}
-			<Borrow vaultSupport={$vaultSupport} />
+			<Borrow vaultSupport={$vaultSupport} {maxBorrowAmount} />
 			<div class="borrow-repay-section__select-box">
 				<button
-					on:click={() => {
-						onTokenListOpen()
-					}}
+					on:click={() => borrowListVisible = true}
 					class="borrow-repay-section__select"
 				>
-					<img src={$vaultSupport.baseTokenInfo.logoURI} alt={$vaultSupport.baseTokenInfo.symbol} />
-					<p>{$vaultSupport.baseTokenInfo.symbol}</p>
+					<img src={baseTokenInfo.logoURI} alt={baseTokenInfo.symbol} />
+					<p>{baseTokenInfo.symbol}</p>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="16"
@@ -66,7 +72,7 @@
 					</svg>
 				</button>
 			</div>
-			<Repay vaultSupport={$vaultSupport} />
+			<Repay vaultSupport={$vaultSupport} {maxRepayAmount} />
 		{/if}
 
 		<div class="borrow-info-section">
@@ -76,8 +82,8 @@
 
 	<TokenList
 		on:onTokenClick={(e) => onTokenClick(e.detail)}
-		on:onClose={() => onCloseTokenList()}
-		visible={$borrowListVisible}
+		on:onClose={() => borrowListVisible = false}
+		visible={borrowListVisible}
 		vaultsSupport={$protocolStateStore.vaultsSupport}
 		withQuote={false}
 	/>

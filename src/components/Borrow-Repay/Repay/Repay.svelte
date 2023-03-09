3ef7cb1e9a2395106411684b3ae95ccb1990d02b
tejks <sarmatys.programmer@gmail.com>
2023-03-09 22:51:48 +0100

    feat: move repay transaction

--- src/components/Borrow-Repay/Repay/Repay.svelte
+++ src/components/Borrow-Repay/Repay/Repay.svelte
@@ -1,102 +1,28 @@
 <script lang="ts">
-	import { BN } from '@project-serum/anchor';
-	import { delay } from 'lodash';
-	import { useRepay } from '$src/tools/instructions/useRepay';
-	import { protocolStateStore, type IVaultSupport } from '$src/stores/protocolStateStore';
-	import { derived, get } from 'svelte/store';
-	import { anchorStore } from '$src/stores/anchorStore';
+	import { loadProtocolState, type IVaultSupport } from '$src/stores/protocolStateStore';
 	import { walletStore } from '$src/stores/walletStore';
 	import { web3Store } from '$src/stores/web3Store';
-	import { loadUserStoreAccounts, userStore } from '$src/stores/userStore';
-	import { ComputeBudgetProgram, PublicKey, Transaction, type AccountMeta } from '@solana/web3.js';
+	import { loadUserStoreAccounts } from '$src/stores/userStore';
+	import type Decimal from 'decimal.js';
+	import { useRepayTransaction } from '$src/tools/transactions/useRepayTransaction';
 
 	import GradientButton from '$components/Buttons/GradientButton/GradientButton.svelte';
 	import DecimalInput from '$components/Inputs/DecimalInput/DecimalInput.svelte';
 
-	import { useCreateStatementProgramAddress } from '$src/tools/web3/useCreateStatementProgramAddress';
-	import { useCreateStatement } from '$src/tools/instructions/useCreateStatement';
-	import { useSignAndSendTransaction } from '$src/tools/wallet/useSignAndSendTransaction';
-	import type Decimal from 'decimal.js';
+	$: ({ connection } = $web3Store);
+	$: ({ publicKey } = $walletStore);
 
 	export let vaultSupport: IVaultSupport;
-	export let maxRepayAmount: Decimal | undefined
+	export let maxRepayAmount: Decimal | undefined;
+	
 	let repayInputValue: number;
 
 	async function onRepayClick() {
-		const anchorCopy = get(anchorStore);
-		const walletCopy = get(walletStore);
-		const web3Copy = get(web3Store);
-		const userStoreCopy = get(userStore);
-		const { vaultsAccounts, vaultsAddress, stateAddress, vaultsSupport } = get(protocolStateStore);
-
-		if (anchorCopy && walletCopy.publicKey && vaultsAccounts && userStoreCopy.statementAddress) {
-			const { program } = anchorCopy;
-			const { publicKey } = walletCopy;
-
-			const tx = new Transaction();
-			const statementProgramAddress = useCreateStatementProgramAddress(program, publicKey);
-
-			if (!userStoreCopy.statement) {
-				const userStatemantAccount = await web3Copy.connection.getAccountInfo(
-					userStoreCopy.statementAddress
-				);
-				if (!userStatemantAccount) {
-					tx.add(await useCreateStatement(program, { payer: walletCopy.publicKey! }));
-				}
-			}
-
-			tx.add(
-				ComputeBudgetProgram.setComputeUnitLimit({
-					units: 1000000
-				})
-			);
-
-			const vaultsToRefresh = userStoreCopy.statement?.vaults_to_refresh();
-			let remainingAccounts: AccountMeta[] = [];
-
-			if (vaultsToRefresh) {
-				const refresh = vaultsToRefresh.filter(
-					(value, index) => vaultsToRefresh.indexOf(value) === index
-				);
-
-				for (const id of refresh) {
-					remainingAccounts.push(
-						{
-							isSigner: false,
-							isWritable: false,
-							pubkey: vaultsSupport[id].baseOracle
-						},
-						{
-							isSigner: false,
-							isWritable: false,
-							pubkey: vaultsSupport[id].quoteOracle
-						}
-					);
-				}
-			}
-
-			tx.add(
-				await useRepay(
-					program,
-					vaultSupport.id,
-					{
-						statement: statementProgramAddress,
-						accountBase: userStoreCopy.getTokenAccountAddress(vaultSupport.baseTokenAddress)!,
-						reserveBase: new PublicKey(vaultsAccounts.base_reserve(vaultSupport.id)),
-						vaults: vaultsAddress,
-						state: stateAddress,
-						signer: publicKey
-					},
-					remainingAccounts,
-					new BN(repayInputValue * 10 ** vaultSupport.baseTokenInfo.decimals)
-				)
-			);
-
-			await useSignAndSendTransaction(web3Copy.connection, walletCopy, tx);
-			delay(async () => {
-				await loadUserStoreAccounts();
-			}, 3000);
-		}
+		const signature = await useRepayTransaction(connection, vaultSupport, repayInputValue);
+		await connection.confirmTransaction(signature, 'confirmed');
+		await loadProtocolState();
+		await loadUserStoreAccounts();
+		repayInputValue = 0;
 	}
 </script>
 
@@ -104,9 +30,12 @@
 	<div class="repay__operation">
 		<div class="repay__operation-box">
 			<div class="borrow__label">
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
 				<span
-					>To repay: {maxRepayAmount?.toString() ?? '--'}</span
-				>
+					on:click={() =>
+						publicKey && maxRepayAmount ? (repayInputValue = maxRepayAmount.toNumber()) : null}
+					>To repay: {maxRepayAmount?.toString() ?? '--'}
+				</span>
 			</div>
 			<div class="repay__input">
 				<DecimalInput bind:value={repayInputValue} />

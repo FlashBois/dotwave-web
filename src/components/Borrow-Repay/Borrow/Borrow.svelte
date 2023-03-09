dbd9f679695f01c2473d081be7f9bdf7e2b669ef
tejks <sarmatys.programmer@gmail.com>
2023-03-09 13:19:55 +0100

    feat: move borrow transaction

--- src/components/Borrow-Repay/Borrow/Borrow.svelte
+++ src/components/Borrow-Repay/Borrow/Borrow.svelte
@@ -1,99 +1,27 @@
 <script lang="ts">
 	import GradientButton from '$components/Buttons/GradientButton/GradientButton.svelte';
-
-	import { protocolStateStore, type IVaultSupport } from '$src/stores/protocolStateStore';
-	import { get } from 'svelte/store';
-	import { anchorStore } from '$src/stores/anchorStore';
-	import { walletStore } from '$src/stores/walletStore';
-	import { web3Store } from '$src/stores/web3Store';
-	import { loadUserStoreAccounts, userStore } from '$src/stores/userStore';
-	import { ComputeBudgetProgram, PublicKey, Transaction, type AccountMeta } from '@solana/web3.js';
-	import { useCreateStatementProgramAddress } from '$src/tools/web3/useCreateStatementProgramAddress';
-	import { useCreateStatement } from '$src/tools/instructions/useCreateStatement';
-	import { useBorrow } from '$src/tools/instructions/useBorrow';
-	import { BN } from '@project-serum/anchor';
-	import { useSignAndSendTransaction } from '$src/tools/wallet/useSignAndSendTransaction';
-	import { delay } from 'lodash';
 	import DecimalInput from '$components/Inputs/DecimalInput/DecimalInput.svelte';
 
+	import type { IVaultSupport } from '$src/stores/protocolStateStore';
+	import { web3Store } from '$src/stores/web3Store';
+	import { loadUserStoreAccounts } from '$src/stores/userStore';
+	import { delay } from 'lodash';
+	import type Decimal from 'decimal.js';
+	import { useBorrowTransaction } from '$src/tools/transactions/useBorrowTransaction';
+
+	$: ({ connection } = $web3Store);
+
 	export let vaultSupport: IVaultSupport;
+	export let maxBorrowAmount: Decimal | undefined;
+
 	let borrowInputValue: number;
 
 	async function onBorrowClick() {
-		const anchorCopy = get(anchorStore);
-		const walletCopy = get(walletStore);
-		const web3Copy = get(web3Store);
-		const userStoreCopy = get(userStore);
-		const { vaultsAccounts, vaultsAddress, stateAddress, vaultsSupport } = get(protocolStateStore);
-
-		if (anchorCopy && walletCopy.publicKey && vaultsAccounts && userStoreCopy.statementAddress) {
-			const { program } = anchorCopy;
-			const { publicKey } = walletCopy;
-
-			const tx = new Transaction();
-			const statementProgramAddress = useCreateStatementProgramAddress(program, publicKey);
-
-			if (!userStoreCopy.statement) {
-				const userStatemantAccount = await web3Copy.connection.getAccountInfo(
-					userStoreCopy.statementAddress
-				);
-				if (!userStatemantAccount) {
-					tx.add(await useCreateStatement(program, { payer: walletCopy.publicKey! }));
-				}
-			}
-
-			tx.add(
-				ComputeBudgetProgram.setComputeUnitLimit({
-					units: 1000000
-				})
-			);
-
-			const vaultsToRefresh = userStoreCopy.statement?.vaults_to_refresh();
-			let remainingAccounts: AccountMeta[] = [];
-
-			if (vaultsToRefresh) {
-				const refresh = vaultsToRefresh.filter(
-					(value, index) => vaultsToRefresh.indexOf(value) === index
-				);
-
-				for (const id of refresh) {
-					remainingAccounts.push(
-						{
-							isSigner: false,
-							isWritable: false,
-							pubkey: vaultsSupport[id].baseOracle
-						},
-						{
-							isSigner: false,
-							isWritable: false,
-							pubkey: vaultsSupport[id].quoteOracle
-						}
-					);
-				}
-			}
-
-			tx.add(
-				await useBorrow(
-					program,
-					vaultSupport.id,
-					{
-						statement: statementProgramAddress,
-						accountBase: userStoreCopy.getTokenAccountAddress(vaultSupport.baseTokenAddress)!,
-						reserveBase: new PublicKey(vaultsAccounts.base_reserve(vaultSupport.id)),
-						vaults: vaultsAddress,
-						state: stateAddress,
-						signer: publicKey,
-					},
-					remainingAccounts,
-					new BN(borrowInputValue * 10 ** vaultSupport.baseTokenInfo.decimals)
-				)
-			);
-
-			await useSignAndSendTransaction(web3Copy.connection, walletCopy, tx);
-			delay(async () => {
-				await loadUserStoreAccounts();
-			}, 3000);
-		}
+		await useBorrowTransaction(connection, vaultSupport, borrowInputValue);
+		
+		delay(async () => {
+			await loadUserStoreAccounts();
+		}, 3000);
 	}
 </script>
 
@@ -101,7 +29,7 @@
 	<div class="borrow__operation">
 		<div class="borrow__operation-box">
 			<div class="borrow__label">
-				<span>Max borrow: -- </span>
+				<span>Max borrow: {maxBorrowAmount?.toString() ?? '--'} </span>
 			</div>
 			<div class="borrow__input">
 				<DecimalInput bind:value={borrowInputValue} />
---
+++ src/tools/transactions/useBorrowTransaction.ts
@@ -0,0 +1,97 @@
+import { anchorStore } from '$src/stores/anchorStore';
+import { protocolStateStore, type IVaultSupport } from '$src/stores/protocolStateStore';
+import { userStore } from '$src/stores/userStore';
+import { walletStore } from '$src/stores/walletStore';
+import { BN } from '@project-serum/anchor';
+import {
+	ComputeBudgetProgram,
+	Connection,
+	PublicKey,
+	Transaction,
+	type AccountMeta
+} from '@solana/web3.js';
+import { get } from 'svelte/store';
+import { useBorrow } from '../instructions/useBorrow';
+import { useCreateStatement } from '../instructions/useCreateStatement';
+import { useSignAndSendTransaction } from '../wallet/useSignAndSendTransaction';
+import { useCreateStatementProgramAddress } from '../web3/useCreateStatementProgramAddress';
+
+export async function useBorrowTransaction(
+	connection: Connection,
+	vaultSupport: IVaultSupport,
+	amount: number
+): Promise<string> {
+	const anchorCopy = get(anchorStore);
+	const walletCopy = get(walletStore);
+	const userStoreCopy = get(userStore);
+	const { vaultsAccounts, vaultsAddress, stateAddress, vaultsSupport } = get(protocolStateStore);
+	const { publicKey } = walletCopy;
+
+	if (anchorCopy && publicKey && vaultsAccounts && userStoreCopy.statementAddress) {
+		const { program } = anchorCopy;
+
+		const tx = new Transaction();
+		const statementProgramAddress = useCreateStatementProgramAddress(program, publicKey);
+
+		if (!userStoreCopy.statement) {
+			const userStatemantAccount = await connection.getAccountInfo(userStoreCopy.statementAddress);
+
+			if (!userStatemantAccount) {
+				tx.add(await useCreateStatement(program, { payer: publicKey }));
+			}
+		}
+
+		tx.add(
+			ComputeBudgetProgram.setComputeUnitLimit({
+				units: 1000000
+			})
+		);
+
+		const vaultsToRefresh = userStoreCopy.statement?.vaults_to_refresh();
+		const remainingAccounts: AccountMeta[] = [];
+
+		if (vaultsToRefresh) {
+			const refresh = vaultsToRefresh.filter(
+				(value, index) => vaultsToRefresh.indexOf(value) === index
+			);
+
+			for (const id of refresh) {
+				remainingAccounts.push(
+					{
+						isSigner: false,
+						isWritable: false,
+						pubkey: vaultsSupport[id].baseOracle
+					},
+					{
+						isSigner: false,
+						isWritable: false,
+						pubkey: vaultsSupport[id].quoteOracle
+					}
+				);
+			}
+		}
+
+		const userAccountBase = userStoreCopy.getTokenAccountAddress(vaultSupport.baseTokenAddress);
+
+		if (userAccountBase) {
+			tx.add(
+				await useBorrow(
+					program,
+					vaultSupport.id,
+					{
+						statement: statementProgramAddress,
+						accountBase: userAccountBase,
+						reserveBase: new PublicKey(vaultsAccounts.base_reserve(vaultSupport.id)),
+						vaults: vaultsAddress,
+						state: stateAddress,
+						signer: publicKey
+					},
+					remainingAccounts,
+					new BN(amount * 10 ** vaultSupport.baseTokenInfo.decimals)
+				)
+			);
+
+			return await useSignAndSendTransaction(connection, walletCopy, tx);
+		} else throw Error('User token account not exists')
+	} else throw Error('Incomplete protocol state')
+}

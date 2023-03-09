fb09cc1f18ccf03fde2a12dd515ddcc3bee3799d
tejks <sarmatys.programmer@gmail.com>
2023-03-09 13:20:35 +0100

    feat: add decimal to getDecimalFromBigint fn

--- src/tools/decimal/getDecimalFromBigInt.ts
+++ src/tools/decimal/getDecimalFromBigInt.ts
@@ -1,7 +1,7 @@
 import Decimal from 'decimal.js';
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export const getDecimalFromBigint = (value: BigInt) => {
-	return new Decimal(value.toString())
+export const getDecimalFromBigint = (value: bigint, decimal = 1) => {
+	if(decimal == 1) return new Decimal(value.toString())
+	else return new Decimal(value.toString()).div(10 ** decimal)
 }
 

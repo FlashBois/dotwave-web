687a980ae97aee0c98721b3672d54ea98b599592
tejks <sarmatys.programmer@gmail.com>
2023-03-11 13:43:13 +0100

    feat: create notifications

---
+++ src/components/Notification/Notifications.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Notification from './Notification.svelte';
+	import { notificationStore } from './notificationsStore';
+
+	export let item: ConstructorOfATypedSvelteComponent;
+</script>
+
+<slot />
+<div class="notifications">
+	<div class="notification-position">
+		{#each $notificationStore as notification}
+			<Notification {notification} {item} />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.notification-position {
+		position: fixed;
+		max-width: 400px;
+		bottom: 0;
+		left: 0;
+	}
+</style>
